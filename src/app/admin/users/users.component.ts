f8e3946ed7ac530d8b1bdeeab149df73eb5b828d
memory1 <mshen_85@hotmail.com>
2019-01-04 15:37:30 +0800

    aa

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  users:any[];
+  hasPermission = false;
   constructor() { }
 
   ngOnInit() {
+    if (this.hasPermission) {
+      this.getUsers().then(users=> this.users = users)
+      .catch(e => console.log(e.message));
+    } else {
+      this.users = [];
+    }
+  }
+  getUsers(): any {
+    return [
+      {name: 'John', email:'john@a.com'},
+      {name:'colleen', email:'c@b.com'}
+    ];
   }
 
 }
